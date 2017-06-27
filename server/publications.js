c79010eddd3f588b14acf6b1234e3ecacfa486a5
apzlx <aprilzhou98@gmail.com>
2017-06-27 13:11:01 -0400

    Add interactive map on home page

--- server/publications.js
+++ server/publications.js
@@ -1,7 +1,7 @@
-Meteor.publish('people',function(){
-  return Bios.find();
-
-})
-Meteor.publish('people',function(){
-  return People.find();
-})
+// Meteor.publish('people',function(){
+//   return Bios.find();
+//
+// })
+// Meteor.publish('people',function(){
+//   return People.find();
+// })
